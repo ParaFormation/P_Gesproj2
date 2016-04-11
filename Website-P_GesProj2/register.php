a0442af4d14b01a986883c45d23fdf6da7309b01
RomainGillieron <gillieron.romain@hotmail.fr>
2016-04-11 12:14:11 +0200

    page d'insrciption et result

--- Website-P_GesProj2/register.php
+++ Website-P_GesProj2/register.php
@@ -18,6 +18,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	<?php include './gesprojClass.php'; ?>
 </head>
 
 <body>
@@ -31,32 +32,25 @@ include './login.php';
     <div class="section">
         <h1>Inscription</h1>
         <div class="row">
-            <form class="col s12">
+            <form class="col s12" method="POST" action="checkRegister.php">
                 <div class="row">
                     <div class="input-field col s12 m6">
                         <i class="material-icons prefix">account_circle</i>
-                        <input id="last_name" type="text" class="validate">
-                        <label for="last_name">Nom</label>
+                        <input name="username" id="username" type="text" class="validate">
+                        <label for="username">Nom d'utilisateur</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12 m6">
-                        <i class="material-icons prefix">email</i>
-                        <input id="email" type="email" class="validate">
-                        <label for="email">E-mail</label>
-                    </div>
-                 </div>
-                <div class="row">
-                    <div class="input-field col s12 m6">
                         <i class="mdi-action-lock-outline prefix"></i>
-                        <input id="password" type="password" class="validate">
+                        <input id="password" name="password" type="password" class="validate">
                         <label for="password">Mot de passe</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12 m6">
                         <i class="mdi-action-lock-outline prefix"></i>
-                        <input id="confirmPassword" type="password" class="validate">
+                        <input id="confirmPassword" name="confirmPassword" type="password" class="validate">
                         <label for="confirmPassword">Confirmation mot de passe</label>
                     </div>
                 </div>
@@ -69,7 +63,7 @@ include './login.php';
 </div>
 
 <?php
-include 'footer.php';
+	include 'footer.php';
 ?>
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
---
+++ Website-P_GesProj2/registerResult.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * User: bijelical
+ * Date: 21.03.2016
+ * Summary: 
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Accueil - P_GesProj2</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+    <?php
+        include './navbar.php';
+    ?>
+ <div class="container">
+    <div class="section">
+        <?php
+        include 'login.php';
+		
+		if(isset($_GET['res']))
+		{
+			if($_GET['res'] == 1)
+			{
+				echo('<div class="card-panel white-text teal lighten-2"><h4>Un nouvel utilisateur a été ajouté avec succès !</h4></div>
+					  <div class="center" ><a class="modal-trigger waves-effect waves-light btn teal lighten-2" href="index.php">Retour à la page d\'accueil</a></div>');
+			}
+			else if($_GET['res'] == 0)
+			{
+				echo('<div class="card-panel white-text red darken-1"><h4>Une erreur est survenue lors de l\'ajout de l\'utilisateur.</h4></div>
+					<div class="center" ><a class="modal-trigger waves-effect waves-light btn red darken-1" href="register.php">Retour à la page d\'inscription</a></div>');
+			}
+			else
+			{
+				header("Location : index.php");
+			}
+		}
+		else
+		{
+			header("Location : index.php");
+		}
+
+		?>
+	</div>
+</div>
+<?php include"footer.php" ?>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
