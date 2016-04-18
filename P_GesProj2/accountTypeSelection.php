6a9abbf7694b1e67f4f1a81c91ef2ce33f205a6f
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-04-18 11:56:10 +0200

    Page pour la sélection du type de compte.

---
+++ P_GesProj2/accountTypeSelection.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+?>
+<?php
+/**
+ * User: bijelical
+ * Date: 21.03.2016
+ * Summary: 
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Qui êtes vous ? - P_GesProj2</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+<main>
+	<!-- Navbar -->
+    <?php include './navbar.php'; ?>
+	
+	<!-- Content section -->
+	<div class="container">	
+		<div class="row">
+			<div class="col s12 m12 center">
+				<h4>Qui êtes vous ?</h4>
+			</div>
+        </div>
+		<div class="row">
+			<div class="col s12 m12 center">
+				<a href="./addStudentInfos.php" class="waves-effect waves-light btn"><i class="material-icons left">assignment_ind</i>Élève</a>
+				<a  href="./addTeacherInfos.php" class="waves-effect waves-light btn"><i class="material-icons left">perm_identity</i>Formateur</a>
+			</div>
+		</div>
+	</div>
+	
+	<!-- Footer -->   
+	<?php include 'footer.php';?>
+		
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+</main>
+  </body>
+</html>
