60d43b1b391c236827355c6ecbbb7a824b53181e
AlenBijelic <bijelicalen@hotmail.ch>
2016-04-18 10:53:34 +0200

    Ajout des classes pour l'optimisation du mode tablette

--- P_GesProj2/navbar.php
+++ P_GesProj2/navbar.php
@@ -13,22 +13,22 @@
                 <a href="index.php" class="brand-logo center"><img src="images/logo.png" alt="Logo"></a>
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul class="left hide-on-med-and-down">
-                    <li><a href="index.php">Accueil</a></li>
-                    <li><a href="formations.php">Formations</a></li>
-                    <li><a href="trainers.php">Formateurs</a></li>
-                    <li><a href="about.php">A propos</a></li>
+                    <li><a href="index.php" class="hide-button">Accueil</a></li>
+                    <li><a href="formations.php" class="hide-button">Formations</a></li>
+                    <li><a href="trainers.php" class="hide-button">Formateurs</a></li>
+                    <li><a href="about.php" class="hide-button">A propos</a></li>
                 </ul>
                 <ul class="right hide-on-med-and-down">
                     <?php
                     if (isset($_SESSION['user']))
                     {
-                        echo '<li><a class="btn grey lighten-1">'.$_SESSION['user'].'</a></li>';
-                        echo '<li><a href="logoutFunction.php"><i class="material-icons">power_settings_new</i></a></li>';
+                        echo '<li><a class="btn grey lighten-1 hide-button">'.$_SESSION['user'].'</a></li>';
+                        echo '<li><a href="logoutFunction.php" class="hide-button"><i class="material-icons">power_settings_new</i></a></li>';
                     }
                     else
                     {
-                        echo'<li><a class="btn grey lighten-1" href="register.php">Inscription</a></li>';
-                        echo'<li><a class="modal-trigger btn light-blue" href="#modal1">Connexion</a></li>';
+                        echo'<li><a class="btn grey lighten-1 hide-button" href="register.php">Inscription</a></li>';
+                        echo'<li><a class="modal-trigger btn light-blue hide-button" href="#modal1">Connexion</a></li>';
                     }
                     ?>
 
