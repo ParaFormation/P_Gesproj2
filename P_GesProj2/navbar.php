f11140050b85f935cd8d7565d12023332d2f0b8a
AlenBijelic <bijelicalen@hotmail.ch>
2016-04-18 09:29:44 +0200

    Ajout d'un logo

--- P_GesProj2/navbar.php
+++ P_GesProj2/navbar.php
@@ -10,7 +10,7 @@
     <div class="navbar-fixed">
         <nav class="blue">
             <div class="nav-wrapper container">
-                <a href="index.php" class="brand-logo center">P_GesProj2</a>
+                <a href="index.php" class="brand-logo center"><img src="images/logo.png" alt="Logo"></a>
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul class="left hide-on-med-and-down">
                     <li><a href="index.php">Accueil</a></li>
@@ -39,18 +39,8 @@
                     <li><a href="trainers.php">Formateurs</a></li>
                     <li><a href="about.php">A propos</a></li>
                     <li class="divider"></li>
-                    <?php
-                    if (isset($_SESSION['user']))
-                    {
-                        echo '<li><a href="logoutFunction.php">Se déconnecter</a></li>';
-                    }
-                    else
-                    {
-                        echo'<li><a class="modal-trigger waves-effect waves-light" href="#modal1">Connexion</a></li>
-                             <li><a class="waves-effect waves-light" href="register.php">Inscription</a></li>';
-                    }
-                    ?>
-
+                    <li><a class="modal-trigger waves-effect waves-light" href="#modal1">Connexion</a></li>
+                    <li><a class="waves-effect waves-light" href="register.php">Inscription</a></li>
                 </ul>
             </div>
         </nav>
