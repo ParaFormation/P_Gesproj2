77ce0f24e8b92fdb3066f70a0e8ab04e6af168e3
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-04-25 08:14:05 +0200

    PHP to check if the user has already configured his profile

---
+++ P_GesProj2/checkIfConfigured.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: P4R4DiSi4C
+ * Date: 21.04.2016
+ * Time: 18:50
+ */
+include "./gesprojClass.php";
+
+if(isset($_SESSION['user']))
+{
+    $class = new gesprojClass();
+    if($class->checkAlreadyRegistered(3) == false)
+    {
+        header("location:accountTypeSelection.php");
+    }
+    else
+    {
+        if($class->checkAlreadyRegistered(1) == true)
+        {
+            header("location:addStudentInfos.php");
+        }
+        else
+        {
+            header("location:addTeacherInfos.php");
+        }
+    }
+}
+else
+{
+    echo 'Pas connecté';
+}
