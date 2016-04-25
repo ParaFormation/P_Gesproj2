57870df3a53390485238531446e006c324cc9bc6
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-04-25 08:12:28 +0200

    Added php in th page.

--- P_GesProj2/addStudentInfos.php
+++ P_GesProj2/addStudentInfos.php
@@ -23,64 +23,89 @@ session_start();
 <body>
 <main>
 	<!-- Navbar -->
-    <?php include './navbar.php'; ?>
-	
-		<!-- Form section -->
-	<div class="container">
-		<div class="section">
-			<h3>Profil élève</h3></br></br>
-				<div class="row">
-					<form class="col s12 m12" method="POST" action="checkRegister.php">
-						<div class="row">
-							<div class="input-field col s6 m6">
-								<i class="material-icons prefix">account_circle</i>
-								<input name="firstname" id="firstname" type="text" class="validate" required>
-								<label for="firstname">Prénom</label>
-							</div>
-							<div class="input-field col s6 m6">
-								<input id="lastname" name="lastname" type="text" class="validate" required>
-								<label for="lastname">Nom de famille</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12 m12">
-								<i class="material-icons prefix">room</i>
-								<input id="address" name="address" type="text" class="validate" required>
-								<label for="address">Adresse + NPA</label>
-							</div>
-							</br></br></br>
-						</div>
-
-					   <h5>Changer le mot de passe</h5>
-							<div class="row">
-								<div class="input-field col s12 m6">
-									<i class="mdi-action-lock-outline prefix"></i>
-									<input id="newPassword" name="newPassword" type="password" class="validate" required>
-									<label for="newPassword">Nouveau mot de passe</label>
-								</div>
-							</div>
-							<div class="row">
-								<div class="input-field col s12 m6">
-									<i class="mdi-action-lock-outline prefix"></i>
-									<input id="confirmNewPassword" name="confirmNewPassword" type="password" class="validate" required>
-									<label for="confirmNewPassword">Confirmation du nouveau mot de passe</label>
-								</div>
-						    </div>
-					<button class="btn waves-effect waves-light blue" type="submit" name="action">Enregistrer les informations
-						<i class="material-icons right">done</i>
-					</button>
-				</form>
-			</div>
-		</div>
-	</div>
-	
-        
- 
+    <?php
+    ob_start();
+        include './gesprojClass.php';
+        include './navbar.php';
+    $newUser = new gesprojClass();
+    if(isset($_SESSION['user']))
+    {
+	echo'
+        <div class="container">
+            <div class="section">
+                <h3>Profil élève</h3></br></br>
+                    <div class="row">
+                        <form class="col s12 m12" method="POST">';
+                                    if($newUser->checkAlreadyRegistered(1) == false)
+                                    {
+                                        echo'
+                                        <div class="row">
+                                            <div class="input-field col s6 m6">
+                                                <i class="material-icons prefix">account_circle</i>
+                                                <input name="firstname" id="firstname" type="text" class="validate" required>
+                                                <label for="firstname">Prénom</label>
+                                            </div>
+                                            <div class="input-field col s6 m6">
+                                                <input id="lastname" name="lastname" type="text" class="validate" required>
+                                                <label for="lastname">Nom de famille</label>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="input-field col s12 m12">
+                                                <i class="material-icons prefix">room</i>
+                                                <input id="address" name="address" type="text" class="validate" required>
+                                                <label for="address">Adresse + NPA</label>
+                                            </div>
+                                            </br></br></br>
+                                        </div>';
+                                    }
+                            if($newUser->checkAlreadyRegistered(1) == true)
+                            {
+                                echo'
+                                <h5> Changer le mot de passe </h5>
+                                <div class="row" >
+                                    <div class="input-field col s12 m6" >
+                                        <i class="mdi-action-lock-outline prefix" ></i >
+                                        <input id = "newPassword" name = "newPassword" type = "password" class="validate" >
+                                        <label for="newPassword" > Nouveau mot de passe </label >
+                                    </div >
+                                </div >
+                                <div class="row" >
+                                    <div class="input-field col s12 m6" >
+                                        <i class="mdi-action-lock-outline prefix" ></i >
+                                        <input id = "confirmNewPassword" name = "confirmNewPassword" type = "password" class="validate" >
+                                        <label for="confirmNewPassword" > Confirmation du nouveau mot de passe </label >
+                                    </div >
+                                </div >';
+                            }
+                        echo'
+                        <button class="btn waves-effect waves-light blue" type="submit" name="action">Enregistrer les informations
+                            <i class="material-icons right">done</i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>';
+    }
+    else
+    {
+        echo 'Pas connecté !';
+    }
+    ?>
 
 	<!-- Footer -->   
-    <?php include 'footer.php';?>
-    
+    <?php
+        include 'footer.php';
+        if(isset($_POST['lastname'],$_POST['firstname'],$_POST['address']))
+        {
+            $newUser->setAddress($_POST['address']);
+            $newUser->setFirstname($_POST['lastname']);
+            $newUser->setLastname($_POST['firstname']);
+            $newUser->newStudForm(0);
 
+            ob_end_flush();
+        }
+    ?>
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
