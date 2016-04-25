5a8217187081bf4e21c13a0ef5b36fc7b5a7c543
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-04-25 08:13:22 +0200

    Added methods for the configuration of the profile.

--- P_GesProj2/gesprojClass.php
+++ P_GesProj2/gesprojClass.php
@@ -6,11 +6,71 @@
  */
 class gesprojClass
 {
+    public $lastname;
+    public $address;
+    public $firstname;
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
     /**
      * Default constructor
      */
     public function __construct() {
         $this->dbh = new PDO('mysql:host=localhost;dbname=db_gesproj2;charset=utf8',"gesproj2", "gesproj2admin",array(PDO::ATTR_PERSISTENT => true,PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    }
+
+    /**
+     * Destructor auto
+     */
+    public function __destruct()
+    {
+        $this->dbh = null;
     }
 
     /**
@@ -146,5 +206,146 @@ class gesprojClass
         return $formers;
     }
 
+    /**
+     * @param $whatType
+     * Method to configure a new student or teacher
+     */
+    public function newStudForm($whatType)
+    {
+        #If a student is asked
+        if($whatType == 0) {
+            $firstname = $this->getFirstname();
+            $lastname = $this->getLastname();
+            $address = $this->getAddress();
+            $id = $this->getSessionID();
+
+            //Check if the id is not null
+            if ($id != null) {
+                //Query to create a new user (username & password)
+                $newStudent = "INSERT INTO t_student(
+                        stuFirstname,
+                        stuLastname,
+                        stuAddress,
+                        fkUser
+                        ) VALUES (
+                        :stuFirstname,
+                        :stuLastname,
+                        :stuAddress,
+                        :fkUser
+                        )";
+
+                //Prepare the query $newUser
+                $stmt = $this->dbh->prepare($newStudent);
+
+                //Bind the parameter $useUsername & userPassword
+                $stmt->bindParam(':stuFirstname', $firstname, PDO::PARAM_STR);
+                $stmt->bindParam(':stuLastname', $lastname, PDO::PARAM_STR);
+                $stmt->bindParam(':stuAddress', $address, PDO::PARAM_STR);
+                $stmt->bindParam(':fkUser', $id, PDO::PARAM_INT);
+
+                //Execute the query
+                $stmt->execute();
+
+
+                //Validate the acc
+                $isStudent = 1;
+                $validateStudent = "UPDATE t_user SET isStudent = '$isStudent' WHERE idUser = '$id'";
+
+                $stmt = $this->dbh->prepare($validateStudent);
+
+                $stmt->execute();
+
+                header("location:index.php");
+            }
+        }
+    }
+
+    /**
+     * @return null
+     * Method to get the id of the session
+     */
+    public function getSessionID()
+    {
+        $username = $_SESSION['user'];
+
+        //Prepare the select request
+        $stmt = $this->dbh->prepare('SELECT idUser FROM t_user WHERE useUsername = ?');
+        $stmt->bindParam(1,$username);
+        //Execute the request
+        $stmt->execute();
+
+        $count = $stmt->rowCount();
+           if($count != 0)
+            {
+                //Get the result of the request in an array
+                $result = $stmt->fetchAll();
+
+                $id = $result[0][0];
+
+                return $id;
+            }
+        return null;
+    }
+
+    /**
+     * @param $isStudent
+     * @return bool
+     * Method to check if a teacher/student has already been configured
+     */
+    public function checkAlreadyRegistered($isStudent)
+    {
+        //Check if the user is logged in
+        if (isset($_SESSION['user'])) {
+            $id = $this->getSessionID();
+
+            //CHECK FOR STUDENT
+            if ($isStudent == 1) {
+                $sql2 = "SELECT idStudent FROM t_student WHERE fkUser = '$id'";
+                $stmt = $this->dbh->prepare($sql2);
+                $stmt->execute();
+                $count = $stmt->rowCount();
+
+                if ($count == 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+            }
+
+            //CHECK FOR TEACHER
+
+
+            //CHECK FOR BOTH
+            if ($isStudent == 3) {
+                $sql2 = "SELECT idStudent FROM t_student WHERE fkUser = '$id'";
+                $stmt = $this->dbh->prepare($sql2);
+                $stmt->execute();
+                $count = $stmt->rowCount();
+
+                if ($count == 0)
+                {
+                    $sql3 = "SELECT idFormer FROM t_former WHERE fkUser = '$id'";
+                    $stmt = $this->dbh->prepare($sql3);
+                    $stmt->execute();
+                    $count = $stmt->rowCount();
+
+                    if($count == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                else
+                {
+                    return true;
+                }
+
+            } else {
+                echo 'Pas connecté !';
+            }
+        }
+        return null;
+    }
 }
 ?>
