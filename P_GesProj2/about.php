0887285f2ac8275b5f01c508964f0e10ca3878d8
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-05-05 15:44:40 +0200

    Suppression balises main

--- P_GesProj2/about.php
+++ P_GesProj2/about.php
@@ -20,8 +20,6 @@ session_start();
     <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
-<body>
-<main>
 <?php
 include './navbar.php';
 ?>
@@ -58,6 +56,4 @@ include 'footer.php';
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="js/materialize.js"></script>
 <script src="js/init.js"></script>
-</main>
-</body>
 </html>
