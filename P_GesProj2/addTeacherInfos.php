cffb139fc806a24e625dc719c7cd5d1d6f0d4649
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-05-05 15:45:08 +0200

    Suppression balises main

--- P_GesProj2/addTeacherInfos.php
+++ P_GesProj2/addTeacherInfos.php
@@ -24,7 +24,6 @@ ob_start();
     <script type="text/javascript" src="./js/jsGesProj.js"></script>
     <script src="./js/materialize.js"></script>
 </head>
-<body>
 	<!-- Navbar -->
     <?php
     ob_start();
@@ -128,9 +127,6 @@ ob_start();
 
 
   <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
 
-  </body>
 </html>
