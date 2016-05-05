f7037bdfd91c5f88fcd33a3d9deac867968b18ac
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-05-05 15:44:56 +0200

    Suppression balises main

--- P_GesProj2/accountTypeSelection.php
+++ P_GesProj2/accountTypeSelection.php
@@ -20,8 +20,7 @@ session_start();
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
-<body>
-<main>
+
 	<!-- Navbar -->
     <?php
     include "./gesprojClass.php";
@@ -32,7 +31,7 @@ session_start();
         echo'
         <div class="container" >
             <div class="row" >
-                <div class="col s12 m12 center" >
+                <div class="col s12 m12 center " >
                     <h4 > Qui êtes vous ?</h4 >
                 </div >
             </div >
@@ -58,6 +57,5 @@ session_start();
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
-</main>
-  </body>
+
 </html>
