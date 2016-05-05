7d25271bbefdaa91e81c51df76207a57379e8535
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-05-05 15:46:58 +0200

    Suppression balises main

--- P_GesProj2/formationsDetails.php
+++ P_GesProj2/formationsDetails.php
@@ -2,7 +2,7 @@
 /**
  * User: bijelical
  * Date: 02.05.2016
- * Summary: 
+ * Summary:
  */
 
 session_start();
@@ -30,11 +30,10 @@ session_start();
     <script type="text/javascript" src="./js/jsGesProj.js"></script>
     <script src="./js/materialize.js"></script>
 </head>
-<body>
-<main>
+
     <?php
-        include './navbar.php';
-        include 'loginModal.php';
+    include './navbar.php';
+    include 'loginModal.php';
     ?>
 
     <div class="container">
@@ -42,21 +41,21 @@ session_start();
             <h1>Mes formations</h1>
             <div class="row">
 
-                    <?php
-                    include 'gesProjClass.php';
+                <?php
+                include './gesprojClass.php';
 
-                    $class = new gesprojClass();
+                $class = new gesprojClass();
 
-                    $result = $class->checkAlreadyRegistered(1);
+                $result = $class->checkAlreadyRegistered(1);
 
-                    if(isset($_SESSION['user']) || $result == true) {
-                        echo    '<ul class="collapsible" data-collapsible="expandable">';
-                        $formations = $class->getTrainerFormations($_SESSION['user']);
+                if(isset($_SESSION['user']) || $result == true) {
+                    echo    '<ul class="collapsible" data-collapsible="expandable">';
+                    $formations = $class->getTrainerFormations($_SESSION['user']);
 
-                        foreach ($formations as $form) {
-                            $nbParticipants = $class->registeredParticipants($form['idTraining']);
+                    foreach ($formations as $form) {
+                        $nbParticipants = $class->registeredParticipants($form['idTraining']);
 
-                            $li = '<li>
+                        $li = '<li>
                                 <div class="collapsible-header active"><i class="material-icons">filter_drama</i>' . $form['traName'] . '</div>
                                 <div class="collapsible-body">
                                     <ul></br>
@@ -79,13 +78,13 @@ session_start();
 
                             </li>';
 
-                            echo($li);;
+                        echo($li);;
 
-                        }
-                        echo '</ul>';
                     }
-                    else{
-                        echo '
+                    echo '</ul>';
+                }
+                else{
+                    echo '
                           <div class="row center">
                             <div class="col s12 m12">
                               <div class="card red">
@@ -100,11 +99,9 @@ session_start();
                             </div>
                           </div>
                         ';
-                    }
-                    ?>
-
+                }
+                ?>
             </div>
-
         </div>
     </div>
     <br><br>
@@ -112,10 +109,6 @@ session_start();
     <?php
     include 'footer.php';
     ?>
-
-
     <!--  Scripts-->
     <script src="js/init.js"></script>
-</main>
-</body>
 </html>
