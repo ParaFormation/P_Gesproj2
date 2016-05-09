0f4a3cc1179dd434afbc1df0c5bc50aaec2b6093
David Carvalho <paradisiac.web@hotmail.ch>
2016-05-09 10:42:29 +0200

    Rajout des regex de théo

--- P_GesProj2/addStudentInfos.php
+++ P_GesProj2/addStudentInfos.php
@@ -6,38 +6,37 @@ ob_start();
 /**
  * User: bijelical
  * Date: 21.03.2016
- * Summary: 
+ * Summary:
  */
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Infos élèves - P_GesProj2</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title>Infos élèves - P_GesProj2</title>
 
-  <!-- CSS  -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="./js/jsGesProj.js"></script>
     <script src="./js/materialize.js"></script>
 </head>
-	<!-- Navbar -->
+<body>
+    <!-- Navbar -->
     <?php
     ob_start();
-        include './gesprojClass.php';
-        include './navbar.php';
+    include './gesprojClass.php';
+    include './navbar.php';
     $newUser = new gesprojClass();
     if(isset($_SESSION['user']) && ($newUser->checkAlreadyRegistered(2) == false))
     {
         echo '
         <div class="container">
             <div class="section">
-                <h3>Profil élève</h3>
-                    <div class="divider"></div>
-                    </br></br>
+                <h3>Profil élève</h3></br></br>
                     <div class="row">
                         <form class="col s12 m12" method="POST" onsubmit="return checkPasswords()" action="post/postStudConfig.php">';
         ob_end_flush();
@@ -47,23 +46,24 @@ ob_start();
                 <div class="row">
                     <div class="input-field col s6 m6">
                         <i class="material-icons prefix">account_circle</i>
-                        <input name="firstname" id="firstname" type="text" class="validate" required>
+                        <input name="firstname" id="firstname" type="text" class="validate"  pattern="^[a-zA-Z-]*" required>
                         <label for="firstname">Prénom</label>
                     </div>
                     <div class="input-field col s6 m6">
-                        <input id="lastname" name="lastname" type="text" class="validate" required>
+                        <input id="lastname" name="lastname" type="text" class="validate" pattern="^[a-zA-Z-]*" required>
                         <label for="lastname">Nom de famille</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">room</i>
-                        <input id="address" name="address" type="text" class="validate" required>
+                        <input id="address" name="address" type="text" pattern="^([a-zA-Z].*)\d" class="validate" required>
                         <label for="address">Adresse + NPA</label>
                     </div>
                     </br></br></br>
                 </div>';
         }
+
         if($newUser->checkAlreadyRegistered(1) == true)
         {
             $displayRegisteredTraining = new gesprojClass();
@@ -117,9 +117,7 @@ ob_start();
                 echo'Nous ne pouvons pas afficher vos formations car vous n\'êtes inscrit à aucune de nos formations.';
             }
 
-
             echo'
-                <br /><br />
                 <h5> Changer le mot de passe </h5>
                 <div class="row" >
                     <div class="input-field col s12 m6" >
@@ -136,15 +134,14 @@ ob_start();
                     </div >
                 </div >';
         }
-                        echo'
+        echo'
                         <button class="btn waves-effect waves-light blue" type="submit" name="action">Enregistrer les informations
                             <i class="material-icons right">done</i>
                         </button>
                     </form>
                 </div>
             </div>
-        </div>
-        ';
+        </div>';
 
     }
     else
@@ -154,11 +151,12 @@ ob_start();
     }
     ?>
 
-	<!-- Footer -->   
+    <!-- Footer -->
     <?php
-        include 'footer.php';
+    include 'footer.php';
     ?>
 
-  <!--  Scripts-->
-  <script src="js/init.js"></script>
+    <!--  Scripts-->
+    <script src="js/init.js"></script>
+</body>
 </html>
