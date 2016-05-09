5308dab660b6812c47b2ea4b43d7b9717e0c4571
AlenBijelic <bijelicalen@hotmail.ch>
2016-05-09 10:30:05 +0200

    Ajout des inscriptions auxquelles on est inscrit

--- P_GesProj2/addStudentInfos.php
+++ P_GesProj2/addStudentInfos.php
@@ -6,37 +6,38 @@ ob_start();
 /**
  * User: bijelical
  * Date: 21.03.2016
- * Summary:
+ * Summary: 
  */
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-    <title>Infos élèves - P_GesProj2</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Infos élèves - P_GesProj2</title>
 
-    <!-- CSS  -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="./js/jsGesProj.js"></script>
     <script src="./js/materialize.js"></script>
 </head>
-<body>
-    <!-- Navbar -->
+	<!-- Navbar -->
     <?php
     ob_start();
-    include './gesprojClass.php';
-    include './navbar.php';
+        include './gesprojClass.php';
+        include './navbar.php';
     $newUser = new gesprojClass();
     if(isset($_SESSION['user']) && ($newUser->checkAlreadyRegistered(2) == false))
     {
         echo '
         <div class="container">
             <div class="section">
-                <h3>Profil élève</h3></br></br>
+                <h3>Profil élève</h3>
+                    <div class="divider"></div>
+                    </br></br>
                     <div class="row">
                         <form class="col s12 m12" method="POST" onsubmit="return checkPasswords()" action="post/postStudConfig.php">';
         ob_end_flush();
@@ -46,18 +47,18 @@ ob_start();
                 <div class="row">
                     <div class="input-field col s6 m6">
                         <i class="material-icons prefix">account_circle</i>
-                        <input name="firstname" id="firstname" type="text" class="validate"  pattern="^[a-zA-Z-]*" required>
+                        <input name="firstname" id="firstname" type="text" class="validate" required>
                         <label for="firstname">Prénom</label>
                     </div>
                     <div class="input-field col s6 m6">
-                        <input id="lastname" name="lastname" type="text" class="validate" pattern="^[a-zA-Z-]*" required>
+                        <input id="lastname" name="lastname" type="text" class="validate" required>
                         <label for="lastname">Nom de famille</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12 m12">
                         <i class="material-icons prefix">room</i>
-                        <input id="address" name="address" type="text" pattern="^([a-zA-Z].*)\d" class="validate" required>
+                        <input id="address" name="address" type="text" class="validate" required>
                         <label for="address">Adresse + NPA</label>
                     </div>
                     </br></br></br>
@@ -65,7 +66,60 @@ ob_start();
         }
         if($newUser->checkAlreadyRegistered(1) == true)
         {
+            $displayRegisteredTraining = new gesprojClass();
+
+            $getUserID = $displayRegisteredTraining->getSessionID();
+
+            $result = $displayRegisteredTraining->getRegisteredFormations($getUserID);
+
+            if(isset($result))
+            {
+                echo'
+                <div class="row">
+                    <h5>Mes inscriptions</h5>
+                    <br />
+                    <ul class="collapsible" data-collapsible="accordion">
+                ';
+
+                foreach($result as $form)
+                {
+                    $li = '<li>
+                            <div class="collapsible-header"><i class="material-icons">filter_drama</i>' . $form['traName'] . '</div>
+                            <div class="collapsible-body">
+                                <ul></br>
+                                    <div class="margin-ul">
+                                        <li><strong>Description : </strong>' .$form['traDescription']. '</li></br>
+                                        <li><strong>Date de début : </strong>' .$form['traStartDate']. '</li></br>
+                                        <li><strong>Date de fin : </strong>' .$form['traEndDate']. '</li></br>
+                                        <li><strong>Nombre des périodes : </strong>' .$form['traPeriodicity']. '</li></br>
+                                        <li><strong>Prix : </strong>' .$form['traPrice']. '.-</li></br>
+                                        <li><strong>Lieu : </strong>' .$form['traLocality']. '</li></br>
+                                        <li><strong>Nombre de participant min. : </strong>' .$form['traMinParticipants']. '</li></br>
+                                        <li><strong>Nombre de participant max. : </strong>' .$form['traMaxParticipants']. '</li></br>
+                                        <li><strong>Formateur : </strong><a href="./trainers.php">' .$form['forFirstname']. ' ' . $form['forLastname'] . '</a></li></br>
+                                        </br>
+                                    </div>
+                                </ul>
+
+                            </div>
+
+                        </li>';
+
+                    echo($li);
+                }
+
+                echo'
+                </ul>
+                ';
+            }
+            else
+            {
+                echo'Nous ne pouvons pas afficher vos formations car vous n\'êtes inscrit à aucune de nos formations.';
+            }
+
+
             echo'
+                <br /><br />
                 <h5> Changer le mot de passe </h5>
                 <div class="row" >
                     <div class="input-field col s12 m6" >
@@ -82,14 +136,15 @@ ob_start();
                     </div >
                 </div >';
         }
-        echo'
+                        echo'
                         <button class="btn waves-effect waves-light blue" type="submit" name="action">Enregistrer les informations
                             <i class="material-icons right">done</i>
                         </button>
                     </form>
                 </div>
             </div>
-        </div>';
+        </div>
+        ';
 
     }
     else
@@ -99,12 +154,11 @@ ob_start();
     }
     ?>
 
-    <!-- Footer -->
+	<!-- Footer -->   
     <?php
-    include 'footer.php';
+        include 'footer.php';
     ?>
 
-    <!--  Scripts-->
-    <script src="js/init.js"></script>
-</body>
+  <!--  Scripts-->
+  <script src="js/init.js"></script>
 </html>
